bb8fe50c65d0f17d8479e0389d07e79db986180c
Brandon Cheng <gluxon@users.noreply.github.com>
2022-07-04 02:33:41 -0400

    Allow run process async util to be canceled

--- Sources/GpgTapNotifier/AsyncUtils.swift
+++ Sources/GpgTapNotifier/AsyncUtils.swift
@@ -10,17 +10,25 @@ enum RunProcessError: Error {
 
 /// Wraps Process.run to make it an async function. Standard out and error are not captured.
 func run(_ url: URL, arguments: [String]) async throws {
-    try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<(), Error>) in
-        do {
-            try Process.run(url, arguments: arguments) { task in
+    let process = Process()
+    process.executableURL = url
+    process.arguments = arguments
+
+    return try await withTaskCancellationHandler(handler: { process.interrupt() }) {
+        return try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<(), Error>) in
+            process.terminationHandler = { task in
                 if task.terminationStatus == 0 {
                     continuation.resume(returning: ())
                 } else {
                     continuation.resume(throwing: RunProcessError.nonZeroExit(task.terminationStatus))
                 }
             }
-        } catch {
-            continuation.resume(throwing: RunProcessError.execError(error))
+
+            do {
+                try process.run()
+            } catch {
+                continuation.resume(throwing: RunProcessError.execError(error))
+            }
         }
     }
 }
