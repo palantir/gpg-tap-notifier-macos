14973c873435915b669f1ec41f4fcbef72074048
Brandon Cheng <gluxon@users.noreply.github.com>
2022-07-06 02:10:55 -0400

    Implement LocalizedError protocol for RunProcessError

--- Sources/GpgTapNotifier/AsyncUtils.swift
+++ Sources/GpgTapNotifier/AsyncUtils.swift
@@ -8,6 +8,15 @@ enum RunProcessError: Error {
     case nonZeroExit(Int32)
 }
 
+extension RunProcessError : LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .execError(let error): return "The process failed to start: \(error)"
+        case .nonZeroExit(let exitCode): return "Process exited with non-zero code: \(exitCode)"
+        }
+    }
+}
+
 /// Wraps Process.run to make it an async function. Standard out and error are not captured.
 func run(_ url: URL, arguments: [String]) async throws {
     let process = Process()
